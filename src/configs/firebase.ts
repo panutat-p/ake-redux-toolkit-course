c1f6b502a9bec97f73604186638e783a2074ae3e
panutat-p <pp.panutat@gmail.com>
2023-03-09 21:50:55 +0700

    feat: add firebase auth service

---
+++ src/configs/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: 'ake-redux-toolkit-course.firebaseapp.com',
+  projectId: 'ake-redux-toolkit-course',
+  storageBucket: 'ake-redux-toolkit-course.appspot.com',
+  messagingSenderId: '235361509226', // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  appId: '1:235361509226:web:20b91ba319c3c2ad7b4f13',
+  measurementId: 'G-9P56LMYBBS',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
---
+++ src/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, setDoc, doc } from 'firebase/firestore';
+import firebaseApp from '../configs/firebase';
+import { UserCredential } from '@firebase/auth';
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export async function registerUser(
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+): Promise<UserCredential> {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await setDoc(doc(db, 'user', userCredential.user.uid), {
+      firstName: firstName,
+      lastName: lastName,
+      photoUrl: 'https://codingthailand.com/site/img/nopic.png',
+      role: 'member',
+    });
+
+    return userCredential;
+  } catch (e) {
+    throw e;
+  }
+}
